562c7fd25f78d00a07da61a55ccdcf5f7063ee70
vivek singh <viveksinghrajpoot274@gmail.com>
2023-06-15 16:57:32 +0530

    code added

--- Array/75. Sort Colors/code.cpp
+++ Array/75. Sort Colors/code.cpp
@@ -1,1 +1,23 @@
-
+    void sortColors(vector<int>& nums) {
+        //declearing three pointers
+        int i=0,j=nums.size()-1,k=0;
+        while(k<=j){
+            if(nums[k] == 0){
+                //whenever 0 is found 
+                //swapped with the leftmost non-zero
+                swap(nums[i],nums[k]);
+                i++;
+                k++;
+            }
+            else if(nums[k] == 2){
+                // whenever 2 is found
+                //swapped with the rightmost non-two
+                swap(nums[k],nums[j]);
+                j--;
+            }
+            else
+            //if 1 is found 
+            //simply pointer is incremented
+            k++;
+        }
+    }
