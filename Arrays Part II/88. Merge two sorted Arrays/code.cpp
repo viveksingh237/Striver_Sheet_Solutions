6dd8686d6fbd8b76bd2729e613d949810197c217
vivek singh <viveksinghrajpoot274@gmail.com>
2023-07-11 12:37:19 +0530

    Updated code added

--- Arrays Part II/88. Merge two sorted Arrays/code.cpp
+++ Arrays Part II/88. Merge two sorted Arrays/code.cpp
@@ -14,7 +14,7 @@ public:
         //  while(j<n) v.push_back(nums2[j]);
         //  nums1 = v;
 
-        //****** without using extra space
+       // ****** without using extra space
         int i=m-1,j=n-1;
         while(i>=0 and j>=0){
             if(nums1[i] >= nums2[j]){
@@ -29,14 +29,10 @@ public:
        while(j >= 0) nums1[i+j+2] = nums2[j--];
 
        //******* Using sorting
-    //    int i=m-1,j=0;
-    //    while(nums1[i] >= nums2[j]){
-    //        swap(nums1[i],nums2[j]);
-    //        i--;
-    //        j++;
+    //    for(int j = 0,i = m;j < n;j++){
+    //        nums1[i] = nums2[j];
+    //        i++;
     //    }
-    //    sort(nums2.begin(),nums2.end());
-    //    sort(nums1.begin(),nums1.begin()+m);
-    //    for(int k=m,l=0;l<n and k< m+n-1;k++,l++) nums1[k] = nums2[l];
+    //    sort(nums1.begin(),nums1.end());
     }
 };
