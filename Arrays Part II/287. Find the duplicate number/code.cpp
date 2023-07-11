34ffb801269583b11c9e0948d5ce4a9fdff85e79
vivek singh <viveksinghrajpoot274@gmail.com>
2023-07-11 14:09:15 +0530

    code added

--- Arrays Part II/287. Find the duplicate number/code.cpp
+++ Arrays Part II/287. Find the duplicate number/code.cpp
@@ -1,1 +1,29 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        //***usingh sorting O(nlogn)
+        // sort(nums.begin(),nums.end());
+        // for(int i=0;i<nums.size()-1;i++)
+        // if(nums[i] == nums[i+1])
+        // return nums[i];
+        // return nums[0];
+         
+         //****** Using cycle slow and fast pointer
+         int slow = nums[0];
+         int fast = nums[0];
+
+         do{
+             slow = nums[slow];
+             fast = nums[nums[fast]];
+         } while( slow != fast);
+
+         fast = nums[0];
+         while( slow != fast){
+             slow = nums[slow];
+             fast = nums[fast];
+         }
+         return slow;
+
+    }
+};
 
