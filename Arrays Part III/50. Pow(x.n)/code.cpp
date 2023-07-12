9c758d8110420451fb2d4822b173787b9dfc60d0
vivek singh <viveksinghrajpoot274@gmail.com>
2023-07-12 16:35:40 +0530

    optimized code added

--- Arrays Part III/50. Pow(x.n)/code.cpp
+++ Arrays Part III/50. Pow(x.n)/code.cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+      ///using binary exponentiation
+        double ans = 1.00;
+        if(n < 0)
+        x = 1/x;   // in case power is negative
+        n = abs(n);
 
+            while(n){
+             if(n & 1)
+                ans *= x ;
+                
+                x *= x;
+                n = n/2;
+            }
+
+            return ans;
+        }
+};
