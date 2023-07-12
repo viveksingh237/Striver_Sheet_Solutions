372d0a1adc461e268350d295a86473c5b8e6300b
vivek singh <viveksinghrajpoot274@gmail.com>
2023-07-12 17:14:50 +0530

    3 sol with diff approach added

--- Arrays Part III/74. Search in 2D matrix/code.cpp
+++ Arrays Part III/74. Search in 2D matrix/code.cpp
@@ -8,17 +8,38 @@ public:
         
         // return false;
 
-        //*** Kind of binary search in matrix
-        int i = 0;
-        int j = matrix[0].size()-1; // to start from right-top most corner
-        while(j >= 0 and i < matrix.size()){
-            if(matrix[i][j] == target)
+        //*** In linera time O(m+n)
+        // int i = 0;
+        // int j = matrix[0].size()-1; // to start from right-top most corner
+        // while(j >= 0 and i < matrix.size()){
+        //     if(matrix[i][j] == target)
+        //     return true;
+        //     else if(matrix[i][j] > target)
+        //     j--;                             //shift toward left
+        //     else
+        //     i++;                              //shift toward downwards
+        // }
+        // return false;
+
+
+//******** Binary search considering it as 1D array of m*n size
+        int low = 0;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int high = m*n - 1;
+        
+        while(low <= high){
+            int mid = low + (high - low)/2; //calc mid
+            //if found return ture
+            if(matrix[mid/m][mid%m] == target)
             return true;
-            else if(matrix[i][j] > target)
-            j--;                             //shift toward left
+            //if target is less reduce the high
+            else if(matrix[mid/m][mid%m] > target)
+            high = mid - 1;
+            //if target is greater low if increased
             else
-            i++;                              //shift toward downwards
+            low = mid + 1;
         }
-        return false;
+      return false;
     }
 };
