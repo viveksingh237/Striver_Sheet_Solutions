3af99d149e46a769385976adce16dd11e73bbe32
vivek singh <viveksinghrajpoot274@gmail.com>
2023-07-15 21:54:07 +0530

    code added

--- Linked List/237. Delete node in linked list (head is not given)/code.cpp
+++ Linked List/237. Delete node in linked list (head is not given)/code.cpp
@@ -1,1 +1,17 @@
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        // here we are not actually deleting the node 
+        //instead we are copying neat node value into this node and next node is deleted/ignored
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
