ff3d0fea02e133489c7b8ab10b0725370ef0d238
vivek singh <viveksinghrajpoot274@gmail.com>
2023-07-17 01:02:00 +0530

    Update code.cpp

--- Arrays IV/Largest Subarray with 0 sum(GFG)/code.cpp
+++ Arrays IV/Largest Subarray with 0 sum(GFG)/code.cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        //naive approach sort and then count
 
+        //better approach in O(n)
+        int maxi =  0,cnt = 0;
+        for(int i = 0;i < nums.size();i++){
+            if(nums[i] == 1){
+              cnt++;
+              maxi = max(cnt, maxi);
+            }
+        
+            else
+             cnt = 0;
+        }
+            
+        return maxi;
+    }
+};
