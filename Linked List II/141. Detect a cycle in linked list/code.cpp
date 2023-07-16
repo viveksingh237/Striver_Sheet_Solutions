1a56f3745bdf947be1ba03392c740dd85c3a5d65
vivek singh <viveksinghrajpoot274@gmail.com>
2023-07-16 07:55:34 +0530

    code added

--- Linked List II/141. Detect a cycle in linked list/code.cpp
+++ Linked List II/141. Detect a cycle in linked list/code.cpp
@@ -1,1 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+      ///1. CAn do using hashing if pointer saved in hash table occurs again return true
 
+      //2. usng slow and fast pointer
+       ListNode *slow = head, *fast = head;
+       while(fast and fast->next){
+           slow = slow->next;
+           fast = fast->next->next;
+           if(slow == fast) return true;
+       }
+       return false;
+    }
+};
