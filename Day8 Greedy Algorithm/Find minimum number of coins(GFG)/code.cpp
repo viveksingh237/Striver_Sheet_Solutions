22c02f11d6e9e8698bd1337027294bde9f05398a
vivek singh <viveksinghrajpoot274@gmail.com>
2023-07-18 15:55:34 +0530

    Update and rename code.cpp

---
+++ Day8 Greedy Algorithm/Find minimum number of coins(GFG)/code.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    vector<int> minPartition(int amount)
+    {
+    int coins[10] = {2000, 500, 200, 100, 50, 20, 10, 5, 2, 1};
+    vector<int> ans;
+    for(int i = 0;i < 10;i++){
+        int temp = amount/coins[i]; //no of coins calculated maybe 0 also
+        amount = amount - (coins[i]*temp);//reducing the amount 
+        while(temp--) 
+        ans.push_back(coins[i]);  //adding notes to vector
+        
+    }
+    return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        
+        Solution ob;
+        vector<int> numbers = ob.minPartition(N);
+        for(auto u: numbers)
+            cout<<u<<" ";
+        cout<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
