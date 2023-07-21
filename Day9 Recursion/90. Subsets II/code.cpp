c9a6842d274900b004258570f9631b8d2c365ea0
vivek singh <viveksinghrajpoot274@gmail.com>
2023-07-21 09:24:47 +0530

    code added

--- Day9 Recursion/90. Subsets II/code.cpp
+++ Day9 Recursion/90. Subsets II/code.cpp
@@ -1,1 +1,22 @@
-
+class Solution {
+private:
+void solve(vector<int>& nums,int ind,vector<int> subSet,set<vector<int>> &s){
+    if(ind == nums.size()){
+        sort(subSet.begin(),subSet.end());
+        s.insert(subSet);
+        return;
+    }
+    subSet.push_back(nums[ind]);
+    solve(nums,ind + 1,subSet,s);
+    subSet.pop_back();
+    solve(nums,ind + 1,subSet,s);
+}
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        set<vector<int>> s;
+        vector<int> subSet;
+        solve(nums,0,subSet,s);
+        vector<vector<int>> ans(s.begin(),s.end());
+        return ans;
+    }
+};
