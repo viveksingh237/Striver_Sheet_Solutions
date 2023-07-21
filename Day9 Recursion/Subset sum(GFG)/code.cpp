a9879b472731f1b052f31fcc2d6dc84de11a0848
vivek singh <viveksinghrajpoot274@gmail.com>
2023-07-21 09:43:33 +0530

    Update code.cpp

--- Day9 Recursion/Subset sum(GFG)/code.cpp
+++ Day9 Recursion/Subset sum(GFG)/code.cpp
@@ -1,34 +1,51 @@
-class Solution {
-    //optimal usng recursion
-private:
-void solve(vector<int>& nums,int ind,vector<int> subSet,vector<vector<int>> &ans){
-    ans.push_back(subSet);
-    for(int i = ind;i < nums.size();i++){
-        if(i != ind and nums[i] == nums[i - 1]) continue;
-        subSet.push_back(nums[i]);
-        solve(nums,i + 1,subSet,ans);
-        subSet.pop_back();
-    }
-}
-// //Brute force
-// private:
-// void solve(vector<int>& nums,int ind,vector<int> subSet,set<vector<int>> &s){
-//     if(ind == nums.size()){
-//         sort(subSet.begin(),subSet.end());
-//         s.insert(subSet);
-//         return;
-//     }
-//     subSet.push_back(nums[ind]);
-//     solve(nums,ind + 1,subSet,s);
-//     subSet.pop_back();
-//     solve(nums,ind + 1,subSet,s);
-// }
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution
+{
+private://Using recursion
+ void solve(vector<int> &arr,int sum,int n,int ind,vector<int> &ans){
+     if(ind == n){
+         ans.push_back(sum);
+         return;
+     }//pick the element
+     solve(arr,sum+arr[ind],n,ind + 1,ans);
+     //donot pick the element
+     solve(arr,sum,n,ind + 1,ans);
+ }
 public:
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<int> subSet;
-        vector<vector<int>> ans;
-        sort(nums.begin(),nums.end());
-        solve(nums,0,subSet,ans);
+    vector<int> subsetSums(vector<int> arr, int N)
+    {
+        vector<int> ans;
+        solve(arr,0,N,0,ans);
+        sort(ans.begin(),ans.end());
         return ans;
     }
 };
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin>>N;
+        vector<int> arr(N);
+        for(int i = 0 ; i < N ; i++){
+            cin >> arr[i];
+        }
+        Solution ob;
+        vector<int> ans = ob.subsetSums(arr,N);
+        sort(ans.begin(),ans.end());
+        for(auto sum : ans){
+            cout<< sum<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
