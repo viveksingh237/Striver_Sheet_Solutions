f363372e722941a962789144d77820cdc34f2c86
vivek singh <viveksinghrajpoot274@gmail.com>
2023-07-22 13:08:53 +0530

    Update code.cpp

--- Day9 Recursion/39. Combination sum/code.cpp
+++ Day9 Recursion/39. Combination sum/code.cpp
@@ -1,51 +1,24 @@
-//{ Driver Code Starts
-#include<bits/stdc++.h> 
-using namespace std; 
-
-// } Driver Code Ends
-class Solution
-{
-private://Using recursion
- void solve(vector<int> &arr,int sum,int n,int ind,vector<int> &ans){
-     if(ind == n){
-         ans.push_back(sum);
-         return;
-     }//pick the element
-     solve(arr,sum+arr[ind],n,ind + 1,ans);
-     //donot pick the element
-     solve(arr,sum,n,ind + 1,ans);
- }
+class Solution {
+private:
+void solve(vector<int>& candidates,int sum,int ind,vector<int> &subSet,vector<vector<int>> &ans,int target){
+    if(ind == candidates.size()){
+     if(sum == target){   //desired combination
+        ans.push_back(subSet);
+     }
+     return;
+    }
+    if(sum < target){
+    subSet.push_back(candidates[ind]);
+    solve(candidates,sum + candidates[ind],ind,subSet,ans,target); //since we can pick element more than once so we donot inc ind
+    subSet.pop_back();
+    }
+    solve(candidates,sum,ind + 1,subSet,ans,target); //not picking the element
+}
 public:
-    vector<int> subsetSums(vector<int> arr, int N)
-    {
-        vector<int> ans;
-        solve(arr,0,N,0,ans);
-        sort(ans.begin(),ans.end());
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> subSet;
+        solve(candidates, 0, 0, subSet, ans, target);
         return ans;
     }
 };
-
-//{ Driver Code Starts.
-int main()
-{
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int N;
-        cin>>N;
-        vector<int> arr(N);
-        for(int i = 0 ; i < N ; i++){
-            cin >> arr[i];
-        }
-        Solution ob;
-        vector<int> ans = ob.subsetSums(arr,N);
-        sort(ans.begin(),ans.end());
-        for(auto sum : ans){
-            cout<< sum<<" ";
-        }
-        cout<<endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
