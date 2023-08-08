7123273fa00e7a9f06bfed2e0aee77df4c472d34
vivek singh <viveksinghrajpoot274@gmail.com>
2023-08-08 23:31:59 +0530

    Update code.cpp

--- Day7 Linked List & Arrays/42.Trapping Rainwater problem/code.cpp
+++ Day7 Linked List & Arrays/42.Trapping Rainwater problem/code.cpp
@@ -1,21 +1,53 @@
 class Solution {
 public:
     int trap(vector<int>& arr) {
-          int n = arr.size();
-  int prefix[n], suffix[n];
-  prefix[0] = arr[0];
-  for (int i = 1; i < n; i++) {
-    prefix[i] = max(prefix[i - 1], arr[i]);
-  }
-  suffix[n - 1] = arr[n - 1];
-  for (int i = n - 2; i >= 0; i--) {
-    suffix[i] = max(suffix[i + 1], arr[i]);
-  }
-  int waterTrapped = 0;
-  for (int i = 0; i < n; i++) {
-    waterTrapped += min(prefix[i], suffix[i]) - arr[i];
-  }
-  return waterTrapped;
-}
+////******Optimal Approach O(n) time and O(1)space******
+       int left = 0,right = arr.size() - 1;
+       int leftMax = 0,rightMax = 0;
+       int ans = 0;
+       while(left <= right){
+        if(arr[left] <= arr[right]){
+          if(arr[left] >= leftMax) 
+            leftMax = arr[left];
+          else
+            ans += (leftMax - arr[left]);
+
+            left++;
+        }
+        else{
+            if(arr[right] >= rightMax) 
+            rightMax = arr[right];
+          else
+            ans += (rightMax - arr[right]);
+
+            right--;
+        }
+       }
+       return ans;
     }
 };
+
+// //******Using Extra space(prefix_max,suffix_max)********
+// class Solution {
+// public:
+//     int trap(vector<int>& arr) {
+//           vector<int> prf,suf;
+//           int maxi = 0;
+//         for(auto& x : arr){
+//             maxi = max(maxi,x);
+//             prf.push_back(maxi);
+//         }
+//         maxi = 0;
+//         for(int  i = arr.size() - 1;i >= 0;i--){
+//             maxi = max(maxi,arr[i]);
+//             suf.push_back(maxi);
+//         }
+//         reverse(suf.begin(),suf.end());
+//         int ans = 0;
+//         for(int i = 1; i < arr.size() - 1;i++){
+//              int mini = min(prf[i - 1],suf[i + 1]);
+//              ans += max(0, mini - arr[i]);
+//         }
+//         return ans;
+//     }
+// };
